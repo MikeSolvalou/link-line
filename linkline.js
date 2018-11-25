6089ae5b4e0e80c248511ef8cbabc0a457b59266
MikeSolvalou <godinemich@myvuw.ac.nz>
2018-11-25 23:33:02 +1300

    Total redesign.
    Now, when you hover over a link, a single svg that covers the page
    is unhidden. A rect element drawn on this svg serves as the hitbox
    to detect when the mouse leaves the area of the hovered :link. The
    lines to other :links with the same href value are also drawn on
    this one svg.
    
    Saves adding a new svg element for every line, and calculating
    bounding boxes for pairs of boxes to get its bounds right.
    
    However, it still breaks on the sqlitetutorial.net page when not
    moving the mouse slowly. Mouse pointer leaves the hitbox area
    before the mouseleave listener even starts listening -> event is
    missed until you re-enter and re-exit the hitbox.

--- linkline.js
+++ linkline.js
@@ -1,26 +1,77 @@
+/*strategy:
+a hidden svg covers the whole page;
+the hitbox that detects the mouseleave event is persistent;
+it gets repositioned over the :link being hovered over;
+*/
+
 //pack everything into linkline object
 const linkline={
 
-  //add a listener for a mouseenter event on every :link in the document
+  //initialize svg (#-linkline-layer), event handlers, hitbox (#-linkline-hitbox)
   init: function(){
+    //add a handler for a mouseenter event on every :link in the document
     const links=document.querySelectorAll(':link');
     for(var link of links){
       link.addEventListener('mouseenter', this.draw);
     }
+
+    //add svg covering whole document to draw graphics on; initially hidden
+    const svg=document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    svg.id='-linkline-layer';
+    svg.style.display='none';
+    svg.setAttribute('height', document.body.clientHeight);
+    document.body.appendChild(svg);
+
+    //add rect for hitbox to detect when mouse leaves a link
+    const hitbox=document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+    hitbox.id='-linkline-hitbox';
+    svg.appendChild(hitbox);
+
+    //add handler for a mouseleave event on hitbox
+    hitbox.addEventListener('mouseleave', this.undrawAll);
   },
 
 
-  //draw lines between this :link and every other :link with the same href value
+  //hide the svg and undraw graphics
+  undrawAll: function(){
+    //hide -linkline-layer svg
+    var svg=document.getElementById('-linkline-layer');
+    svg.style.display='none';
+
+    //undraw all lines
+    var lines=svg.querySelectorAll('.-linkline-linkline');
+    for(line of lines){
+      svg.removeChild(line);
+    }
+  },
+
+
+  //draw graphics highlighting :links with the same href value as this one
   draw: function(e){
     //get bounding rect for :link being hovered over (e.target)
     //provides top, bottom, left and right values rel to top left of viewport
-    //from https://plainjs.com/javascript/styles/get-the-position-of-an-element-relative-to-the-document-24/
+    //see https://plainjs.com/javascript/styles/get-the-position-of-an-element-relative-to-the-document-24/
     var rect0=e.target.getBoundingClientRect();
 
-    //get static NodeList of all :links with the same href value; includes :link being hovered over
-    var links=document.querySelectorAll(`:link[href="${e.target.href}"]`);
+    //get values indicating how far page has been scrolled
+    //note: scrollY and scrollX not available in IE
+    var scrollY = window.scrollY;
+    var scrollX = window.scrollX;
+
+    //rect0 center, rel to tl corner of viewport
+    var rect0CenterX = (rect0.left + rect0.right)/2;
+    var rect0CenterY = (rect0.top + rect0.bottom)/2;
+
+    //rect0 center, rel to tl corner of document
+    var rect0CenterXreldoc = rect0CenterX + scrollX;
+    var rect0CenterYreldoc = rect0CenterY + scrollY;
+
+    //need refs to svg and hitbox
+    var svg=document.getElementById('-linkline-layer');
+    var hitbox=document.getElementById('-linkline-hitbox');
 
     //draw a line from hovered :link to every other :link with same href value
+    var links=document.querySelectorAll(`:link[href="${e.target.href}"]`);
     for(var link of links){
       //don't draw line from hovered :link to itself
       if(link===e.target)
@@ -30,88 +81,44 @@ const linkline={
       drawLine(rect1);
     }
 
-    //draw an svg over the :link to serve as a hitbox for the mouseleave event
-    // ie, detect when mouse leaves the :link, by detecting when it leaves this svg
-    // can't just use the :link since the other svgs are children of it
-    //TODO: try inserting other svgs between :link and :link's parent so this hitbox isn't needed
-    drawHitbox();
+    //position hitbox over hovered link
+    positionHitbox();
+
+    //show svg
+    svg.style.display='unset';
     //end of draw function, only function definitions past here
 
 
-    //remove all svgs drawn by this script
-    function undraw(){
-      //static NodeList of all svgs to remove
-      const svgs=document.querySelectorAll('svg[class^="-linkline"]');
-
-      for(var svg of svgs)
-        svg.parentNode.removeChild(svg);
-    }
-
-
-    //draw svg that just covers rect0, plus a little extra buffer
-    function drawHitbox(){
-      //create svg element
-      var svg=document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-
-      // set svg's boundaries to rect0's boundaries, plus a buffer
+    //position the hitbox (#-linkline-hitbox) so that it covers the hovered :link
+    function positionHitbox(){
+      //set hitbox's boundaries to rect0's boundaries, plus a buffer
       const buff=2;
-      svg.setAttribute('style', `position:fixed; top:${rect0.top-buff}px;
-        left:${rect0.left-buff}px;`);
-      svg.setAttribute('width', rect0.right - rect0.left + buff + buff);
-      svg.setAttribute('height', rect0.bottom - rect0.top + buff + buff);
-      svg.setAttribute('class', '-linkline-linkhitbox');
 
-      e.target.appendChild(svg);
-
-      svg.addEventListener('mouseleave', undraw);
+      hitbox.setAttribute('x', rect0.left + scrollX - buff);
+      hitbox.setAttribute('y', rect0.top + scrollY - buff);
+      hitbox.setAttribute('width', rect0.right - rect0.left + buff + buff);
+      hitbox.setAttribute('height', rect0.bottom - rect0.top + buff + buff);
     }
 
 
-    //draw line from rect0 to rect1
+    //draw line from center of rect0 to center of rect1
     function drawLine(rect1){
-      //create svg element
-      var svg=document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-
-      // set svg's boundaries to the bounding rectangle that encloses rect0 and rect1
-      //  top, left, bottom and right are rel to top left corner of viewport
-      var top = (rect0.top < rect1.top) ? rect0.top : rect1.top;
-      var bottom = (rect0.bottom > rect1.bottom) ? rect0.bottom : rect1.bottom;
-      var left = (rect0.left < rect1.left) ? rect0.left : rect1.left;
-      var right = (rect0.right > rect1.right) ? rect0.right : rect1.right;
-      var width = right-left; //min 1 ?
-      var height = bottom-top;  //min 1 ?
-
-      svg.setAttribute('style', `position:fixed; top:${top}px; left:${left}px;`);
-      svg.setAttribute('width', width);
-      svg.setAttribute('height', height);
-      svg.setAttribute('class', '-linkline-linkline');
-
-      e.target.appendChild(svg);
-
-      //draw line from center of rect0 to center of rect1
-      // rect centers, rel to tl corner of viewport
-      var rect0CenterX = (rect0.left + rect0.right)/2;
-      var rect0CenterY = (rect0.top + rect0.bottom)/2;
+      //rect1 center, rel to tl corner of viewport
       var rect1CenterX = (rect1.left + rect1.right)/2;
       var rect1CenterY = (rect1.top + rect1.bottom)/2;
 
-      // rect centers, rel to tl corner of svg
-      var rect0CenterXrelsvg = rect0CenterX - left;
-      var rect0CenterYrelsvg = rect0CenterY - top;
-      var rect1CenterXrelsvg = rect1CenterX - left;
-      var rect1CenterYrelsvg = rect1CenterY - top;
+      //rect1 center, rel to tl corner of document
+      var rect1CenterXreldoc = rect1CenterX + scrollX;
+      var rect1CenterYreldoc = rect1CenterY + scrollY;
 
-      // create svg path element for line
+      //create svg path element for line
       var path=document.createElementNS('http://www.w3.org/2000/svg', 'path');
-      path.setAttribute('d', `M ${rect0CenterXrelsvg},${rect0CenterYrelsvg}
-        L ${rect1CenterXrelsvg},${rect1CenterYrelsvg}`);
-      path.setAttribute('fill', 'none');
+      path.setAttribute('d', `M ${rect0CenterXreldoc},${rect0CenterYreldoc}
+        L ${rect1CenterXreldoc},${rect1CenterYreldoc}`);
+      path.setAttribute('class', '-linkline-linkline')
 
-      //  TODO: should set these attributes in css; the default values aren't acceptable
-      path.setAttribute('stroke', 'black');
-      path.setAttribute('stroke-width', '3');
-
-      svg.appendChild(path);
+      //add path to svg; ensure hitbox is last so it gets drawn over the lines
+      svg.insertBefore(path, hitbox);
     }
   }
 };
