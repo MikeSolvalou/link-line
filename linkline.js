20837cee59341fb1319c30b91d4edb4eac116200
MikeSolvalou <godinemich@myvuw.ac.nz>
2018-11-29 17:58:53 +1300

    Made hitbox clickable. Same target as hovered link.
    Wrapped hitbox in svg <a> element.
    
    Also set svg height in js. height:100% in css set it to viewport height,
    not document height.

--- linkline.js
+++ linkline.js
@@ -1,34 +1,46 @@
-/*strategy:
-a hidden svg covers the whole page;
-the hitbox that detects the mouseleave event is persistent;
-it gets repositioned over the :link being hovered over;
+/*
+a hidden svg covers the whole document; graphics are drawn on this svg;
+a hitbox (<rect>) covers the :link, but resides on the svg layer,
+so it can detect when the mouse moves off the link;
+the <rect> is wrapped in an <a> (svg namespace) so it can respond to clicks;
 */
 
 //pack everything into linkline object
 const linkline={
 
-  //initialize svg (#-linkline-layer), event handlers, hitbox (#-linkline-hitbox)
+  //initialize svg, some event handlers, hitbox, and hitbox link
   init: function(){
     //add a handler for a mouseenter event on every :link in the document
     const links=document.querySelectorAll(':link');
     for(var link of links){
-      link.addEventListener('mouseenter', this.draw);
+      link.addEventListener('mouseenter', linkline.draw);
     }
 
     //add svg covering whole document to draw graphics on; initially hidden
     const svg=document.createElementNS('http://www.w3.org/2000/svg', 'svg');
     svg.id='-linkline-layer';
     svg.style.display='none';
-    svg.setAttribute('height', document.body.clientHeight);
+
+    // setting height:100%; in CSS just sets the SVG to the viewport height(!?), not document height
+    // also, document height may grow as images load
+    svg.setAttribute('height', document.body.clientHeight); //incase window has already loaded
+    window.addEventListener('load', function() {           //incase window is still loading
+      svg.setAttribute('height', document.body.clientHeight);
+    });
+
     document.body.appendChild(svg);
 
-    //add rect for hitbox to detect when mouse leaves a link
+    //add clickable rect to act as hitbox to detect when mouse leaves a link, or link is clicked
+    const hitboxLink=document.createElementNS('http://www.w3.org/2000/svg', 'a');
+    hitboxLink.id='-linkline-hitbox-link';
+    svg.appendChild(hitboxLink);
+
     const hitbox=document.createElementNS('http://www.w3.org/2000/svg', 'rect');
     hitbox.id='-linkline-hitbox';
-    svg.appendChild(hitbox);
+    hitboxLink.appendChild(hitbox);
 
-    //add handler for a mouseleave event on hitbox
-    hitbox.addEventListener('mouseleave', this.undrawAll);
+    //add handler for mouseleave event on hitbox
+    hitbox.addEventListener('mouseleave', linkline.undrawAll);
   },
 
 
@@ -43,6 +55,10 @@ const linkline={
     for(line of lines){
       svg.removeChild(line);
     }
+
+    //clear href attribute on hitbox link
+    var hitboxLink=document.getElementById('-linkline-hitbox-link');
+    hitboxLink.setAttribute('href', '');
   },
 
 
@@ -54,9 +70,8 @@ const linkline={
     var rect0=e.target.getBoundingClientRect();
 
     //get values indicating how far page has been scrolled
-    //note: scrollY and scrollX not available in IE
-    var scrollY = window.scrollY;
-    var scrollX = window.scrollX;
+    var scrollY = window.scrollY; //unavailable in IE
+    var scrollX = window.scrollX; //unavailable in IE
 
     //rect0 center, rel to tl corner of viewport
     var rect0CenterX = (rect0.left + rect0.right)/2;
@@ -66,9 +81,10 @@ const linkline={
     var rect0CenterXreldoc = rect0CenterX + scrollX;
     var rect0CenterYreldoc = rect0CenterY + scrollY;
 
-    //need refs to svg and hitbox
+    //need these pointers in-scope at drawLine function call
     var svg=document.getElementById('-linkline-layer');
     var hitbox=document.getElementById('-linkline-hitbox');
+    var hitboxLink=document.getElementById('-linkline-hitbox-link');
 
     //draw a line from hovered :link to every other :link with same href value
     var links=document.querySelectorAll(`:link[href="${e.target.href}"]`);
@@ -84,22 +100,24 @@ const linkline={
     //position hitbox over hovered link
     positionHitbox();
 
+    //set hitboxLink target
+    hitboxLink.setAttribute('href', e.target.href)
+
     //fix for case when cursor leaves hitbox before mouseleave event is being listened for
-    svg.addEventListener('mousemove', function(e){
-      //if cursor is outside hitbox, call undrawAll
-      console.log(`${e.clientX},${e.clientY}`);
-      console.log(`${rect0.left+scrollX},${rect0.top+scrollY}   ${rect0.right+scrollX},${rect0.bottom+scrollY}`);
-
-      if(e.clientX < rect0.left || e.clientX > rect0.right
-        || e.clientY < rect0.top || e.clientY > rect0.bottom){
-          linkline.undrawAll();
-        }
-
-    }, {once:true});
+    svg.addEventListener('mousemove', earlyExitCheck, {once:true});
 
     //show svg
     svg.style.display='unset';
     //end of draw function, only function definitions past here
+
+
+    //if cursor is outside hitbox, call undrawAll; e is a MouseEvent
+    function earlyExitCheck(e){
+      if(e.clientX < rect0.left || e.clientX > rect0.right
+        || e.clientY < rect0.top || e.clientY > rect0.bottom){
+        linkline.undrawAll();
+      }
+    }
 
 
     //position the hitbox (#-linkline-hitbox) so that it covers the hovered :link
@@ -128,10 +146,10 @@ const linkline={
       var path=document.createElementNS('http://www.w3.org/2000/svg', 'path');
       path.setAttribute('d', `M ${rect0CenterXreldoc},${rect0CenterYreldoc}
         L ${rect1CenterXreldoc},${rect1CenterYreldoc}`);
-      path.setAttribute('class', '-linkline-linkline')
+      path.setAttribute('class', '-linkline-linkline');
 
       //add path to svg; ensure hitbox is last so it gets drawn over the lines
-      svg.insertBefore(path, hitbox);
+      svg.insertBefore(path, hitboxLink);
     }
   }
 };
